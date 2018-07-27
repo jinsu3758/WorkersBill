afe709fbb7e86d07d699c96e1961f02acf7003ef
daem0n <daem0n@kidsnote.com>
2018-07-28 00:13:39 +0900

    사업자 - 사업장생성, 조회, 목록 , 로그인 api 추가

--- app/src/main/java/com/example/jinsu/work2/manager/TaskManager.java
+++ app/src/main/java/com/example/jinsu/work2/manager/TaskManager.java
@@ -3,12 +3,31 @@ package com.example.jinsu.work2.manager;
 import android.support.annotation.NonNull;
 
 import com.example.jinsu.work2.common.BaseApplication;
+import com.example.jinsu.work2.network.model.Company;
 import com.example.jinsu.work2.network.model.EmailVerify;
 import com.example.jinsu.work2.network.model.Join;
+import com.example.jinsu.work2.network.model.Login;
+import com.example.jinsu.work2.network.model.User;
+
+import java.util.ArrayList;
 
 import retrofit.Callback;
 
 public class TaskManager {
+
+    /**
+     * 내정보
+     */
+    public static void api_get_info(@NonNull Callback<User> callback) {
+        BaseApplication.mApiService.get_me_info(callback);
+    }
+
+    /**
+     * 로그인
+     */
+    public static void login(Login login, @NonNull Callback callback) {
+        BaseApplication.mApiService.login(login, callback);
+    }
 
     /**
      * 이메일 중복검사
@@ -44,4 +63,27 @@ public class TaskManager {
     public static void api_find_address(String query, @NonNull Callback callback) {
         BaseApplication.mApiService.api_find_address(query, callback);
     }
+
+    /**
+     * 사업장 생성
+     */
+    public static void api_company_create(@NonNull Company company,
+                                          @NonNull Callback<Company> callback) {
+        BaseApplication.mApiService.company_create(company, callback);
+    }
+
+    /**
+     * 사업장 상세보기
+     */
+    public static void api_company_list(Callback<ArrayList<Company>> callback) {
+        BaseApplication.mApiService.company_list(callback);
+    }
+
+    /**
+     * 사업장 리스트
+     */
+    public static void api_company_read(int companyId, @NonNull Callback<Company> callback) {
+        BaseApplication.mApiService.company_read(companyId, callback);
+    }
+
 }
--- app/src/main/java/com/example/jinsu/work2/network/ApiService.java
+++ app/src/main/java/com/example/jinsu/work2/network/ApiService.java
@@ -1,12 +1,15 @@
 package com.example.jinsu.work2.network;
 
 
+import com.example.jinsu.work2.network.model.Company;
 import com.example.jinsu.work2.network.model.DocumentList;
 import com.example.jinsu.work2.network.model.EmailVerify;
 import com.example.jinsu.work2.network.model.Join;
 import com.example.jinsu.work2.network.model.Login;
 import com.example.jinsu.work2.network.model.LoginResponse;
+import com.example.jinsu.work2.network.model.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import retrofit.Callback;
@@ -17,6 +20,12 @@ import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface ApiService {
+
+    ///------------------------------
+    /// 내 정 보
+    ///------------------------------
+    @GET("/v1/me")
+    void get_me_info(Callback<User> cb);
 
     @POST("/v1/auth/token/")
     void login(@Body Login req, Callback<LoginResponse> cb);
@@ -35,5 +44,17 @@ public interface ApiService {
 
     @GET("/v1/tools/address")
     void api_find_address(@Query("query") String query, Callback<DocumentList> cb);
+
+    ///------------------------------
+    /// 내 정 보 - 사업장
+    ///------------------------------
+    @POST("/v1/me/companies")
+    void company_create(@Body Company company, Callback<Company> cb);
+
+    @GET("/v1/me/companies")
+    void company_list(Callback<ArrayList<Company>> cb);
+
+    @GET("/v1/me/companies/{companyId}")
+    void company_read(@Path("companyId") int companyid, Callback<Company> cb);
 
 }
